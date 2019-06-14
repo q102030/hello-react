86936305a460de5e12e51bbb6e3897a41afc35f3
q102030 <wang15951.gmail.com>
2019-06-14 17:33:05 +0800

    no message

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React ,{Component} from 'react';
+import Header from './components/Header';
+import Home from './components/Home';
+
 import logo from './logo.svg';
 import './App.css';
 import { directive } from '@babel/types';
@@ -6,13 +9,23 @@ import { directive } from '@babel/types';
 class App extends Component {
   render(){
   return (
-  <div className="container">
+    <div className="container">
+    <div className="row">
+      <div className="col-xs-1 col-xs-offset-11">
+        <Header/>
+      </div>
+    </div>
+    <div className="row">
+      <div className="col-xs-1 col-xs-offset-11">
+        <Home/>
+      </div>
+    </div>
     <div className="row">
       <div className="col-xs-1 col-xs-offset-11">
         <h1>hello!!</h1>
       </div>
     </div>
-  </div>
+    </div>
   );
   }
 }
